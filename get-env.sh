f5c7b48d191c49aeaabc20766dbadb50eb4f030e
Yuri Vlasov <yury.vlasov@dataart.com>
2026-02-05 18:20:37 +0100

    improve scripts

--- get-env.sh
+++ get-env.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-
 export MY_PUBLIC_IP=$(dig @resolver1.opendns.com -t A -4 myip.opendns.com +short)
 
 export IF_NAME='gre1'
+export FW_MARK='0x15'
+export TABLE_NAME='tableX'
 # RU YACLOUD
 export RU_HOST_IP=$MY_PUBLIC_IP
 export RU_HOST_IP_LOCAL=10.129.0.24
